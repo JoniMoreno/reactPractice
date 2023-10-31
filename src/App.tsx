b279dc6aefba3d18ec688f1f721c7ca55d277de7
Yehonatan Moreno <jonimoreno2004@gmail.com>
2023-10-31 13:38:56 +0200

    Add some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Image from "./components/Image";
 
-const onClickAlertHandler = () => {
-  return <Alert>You just got Rick Rolled</Alert>;
-};
 
 function App() {
   const [alertVisible, setAlertVisiblity] = useState(false);
 
   return (
     <div>
-      {alertVisible && <Alert onClose={() => setAlertVisiblity(false)}>Rick Rolled</Alert>}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisiblity(false)}>
+          You just got Rick Rolled
+        </Alert>
+      )}
       <Button color="secondary" onClick={() => setAlertVisiblity(true)}>
         My Button
       </Button>
+
+      <a target="_blank" href="https://youtu.be/dQw4w9WgXcQ?si=NfHauiu706UJbO-K&t=1">
+        <Image src={"file:/C:/reactPractice/public/images/RickRoll.jpeg"} alt="Rick Roll you" />
+      </a>
     </div>
   );
 }
